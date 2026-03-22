26ff6b087f291a96e903b5360afe8df097a1fcbb
Shrot DataScx <shrotvj25@gmail.com>
2026-03-22 11:28:10 +0530

    Task3

--- Car inventory system.cpp
+++ Car inventory system.cpp
@@ -1,7 +1,7 @@
 #include<iostream>
 #include<string>
 using namespace std;
-class Car{// by default , private
+class Car{
     // default constructor & destructor
     private:
     float price;
@@ -34,21 +34,68 @@ class Car{// by default , private
     }
 };
 
+class ElectricCar: public Car{
+    public:
+    float battery_range;
+    void get_info(){
+        cout<< "make :"<< make<<endl;
+        cout<< "model :"<< model<<endl;
+        cout<< "battery_range in miles:"<< battery_range<<endl; //override
+        
+}
+};
+
+class SportsCar: public Car{
+    public:
+    float top_speed;
+    void get_info(){
+        cout<< "make :"<< make<<endl;
+        cout<< "model :"<< model<<endl;
+        cout<< "top_speed in mph:"<< top_speed<<endl ;// override
+        
+    
+
+    }
+};
+
+
 int main(){
-    Car Ferrari;
+    SportsCar Ferrari;
     Ferrari.make="Ferrari";
     Ferrari.model="Ferrari 849 Testarossa";
+    Ferrari.top_speed=200;
     Ferrari.get_info();
-    float p;
+    float p_Ferrari;
     cout<<"Enter original price to be set: ";
-    cin>>p;
-    Ferrari.set_Price(p) ;
-    float percent_discount;
+    cin>>p_Ferrari;
+    Ferrari.set_Price(p_Ferrari) ;
+    float percent_discount_Ferrari;
     cout<<"Enter percentage discount to be applied: ";
-    cin>> percent_discount;
+    cin>> percent_discount_Ferrari;
     cout<<"original price is:"<<Ferrari.get_Price()<<endl;
-    cout<<"Discounted price is:"<<Ferrari.getDiscountedPrice(percent_discount)<<endl;
+    cout<<"Discounted price is:"<<Ferrari.getDiscountedPrice(percent_discount_Ferrari)<<endl;
     // Ferrari.price  >>>would show error that its not accessible as it is private
     // cout<<Ferrari.apply.discount(1.2) >>>would also show error that its not accessible as it is private
 
+    ElectricCar Tesla;
+    Tesla.make="Tesla";
+    Tesla.model="Tesla Model S";
+    Tesla.battery_range=410;
+    Tesla.get_info();
+    float p_Tesla;
+    cout<<"Enter original price to be set: ";
+    cin>>p_Tesla;
+    Tesla.set_Price(p_Tesla) ;
+    float percent_discount_Tesla;
+    cout<<"Enter percentage discount to be applied: ";
+    cin>> percent_discount_Tesla;
+    cout<<"original price is:"<<Tesla.get_Price()<<endl;
+    cout<<"Discounted price is:"<<Tesla.getDiscountedPrice(percent_discount_Tesla)<<endl;
+    // Tesla.price  >>>would show error that its not accessible as it is private
+    // cout<<Tesla.apply.discount(1.2) >>>would also show error that its not accessible as it is private
+return 0;
 }
+
+
+
+
