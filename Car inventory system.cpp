83c01d74f1eb0a18d9cc78b46f69976ce7d4f847
Shrot DataScx <shrotvj25@gmail.com>
2026-03-22 10:21:05 +0530

    Task 1

--- Car inventory system.cpp
+++ Car inventory system.cpp
@@ -1,3 +1,30 @@
 #include<iostream>
 #include<string>
 using namespace std;
+class Car{// by default , private
+    // default constructor & destructor
+    public:
+
+    string make;
+    string model;
+    float price;
+    float apply_discount(float percent_discount){
+       price-= price*(percent_discount)/100;
+       return price;
+    }
+    void get_info(){
+        cout<< "make :"<< make<<endl;
+        cout<< "model :"<< model<<endl;
+        cout<< "price :"<< price<<endl;
+
+    }
+};
+
+int main(){
+    Car Ferrari;
+    Ferrari.make="Ferrari";
+    Ferrari.model="Ferrari 849 Testarossa";
+    Ferrari.price=103700000;
+    Ferrari.get_info();
+
+}
