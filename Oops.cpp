3cd34e8ed577e081e729ba2e45a897c7a55cba48
Shrot DataScx <shrotvj25@gmail.com>
2026-03-22 09:43:49 +0530

    Start of project

---
+++ Oops.cpp
@@ -0,0 +1,28 @@
+// #include<iostream>
+// #include<string>
+// using namespace std;
+// class teacher{
+//     private:
+//     double salary;
+
+//     public:
+//     string name;
+//     string branch;
+//     int age;
+//     void setSalary(double s){
+//         salary=s ;
+//     }
+//     double getSalary(){
+//         cout<< salary<<endl;
+//         return salary;
+//     }
+// };
+
+// int main(){
+//     teacher t1;
+//     t1.setSalary(50);
+//     t1.getSalary();
+//         t1.name="shrot";
+//     cout<<t1.name<<endl;;
+
+// }
